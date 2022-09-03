7528ef4480ae693f9395cdbb8172d15472653b8d
juhwankim-dev <juhwan.dev@gmail.com>
2022-09-03 21:48:26 +0900

    feat: bottom navigation view 구현(#2)

---
+++ app/src/main/java/com/example/mogayo_android/chatroom/ChatRoomFragment.kt
@@ -0,0 +1,9 @@
+package com.example.mogayo_android.chatroom
+
+import com.example.mogayo_android.R
+import com.example.mogayo_android.databinding.FragmentChatRoomBinding
+import com.juhwan.anyang_yi.present.config.BaseFragment
+
+class ChatRoomFragment : BaseFragment<FragmentChatRoomBinding>(R.layout.fragment_chat_room) {
+
+}
